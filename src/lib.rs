e4971a94e34aab5a4a301507430ab4b3c2d384da
Iwaki <valentinperdreau49@gmail.com>
2026-08-21 15:17:38 +0200

    🎉: restarting lib from zero

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-mod update;
-
 #[cfg(test)]
 mod test {
 
-    use crate::{update::updater::Updater};
-
     #[test]
-    fn test_update() {
-        let mut updater = Updater::new("1.20.1");
-        updater.set_relative_local_dir_path(".rll");
-        updater.install_files();
+    pub fn test_lib() {
+        println!("Hello from RustLauncherLib!")
     }
 }
